850e41e7d846928c827bf29f2d4364766b41a64a
menglingfei <787091785@qq.com>
2018-10-16 14:20:17 +0800

    路由，新增组件
    5.1-5.6

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <div id="app">
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      <router-link class="tab-item" to="/goods">商品</router-link>
+      <router-link class="tab-item" to="/ratings">评价</router-link>
+      <router-link class="tab-item" to="/seller">商家</router-link>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-
 <script>
+import header from './components/header/header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import './common/stylus/mixin.styl'
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      border-1px(rgba(7, 17, 27, 0.1))
+      .tab-item
+        flex: 1
+        text-align: center
+        text-decoration: none
+        font-size: 14px
+        color: rgb(77, 85, 93)
+      .active
+        color: rgb(240, 20, 20)
 </style>
